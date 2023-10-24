e25b536cd4017b09b9f32134a26bcafbf9436f0e
luuicdx9 <luiscdx9@gmail.com>
2023-10-24 16:01:24 -0300

    sout print 2

--- app/src/main/java/com/example/clasevideo24_10/MainActivity.java
+++ app/src/main/java/com/example/clasevideo24_10/MainActivity.java
@@ -13,10 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("hello world");
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
 
 
 
